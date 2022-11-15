d6f302359b733f706583e854fe15989e7b54eb80
Johan <johan.moller040@gmail.com>
2022-11-15 17:51:09 +0100

    testing type animations

--- choice.js
+++ choice.js
@@ -3,21 +3,13 @@ import { height, width } from "./game.js";
 import { addsTextToCanvas, isColliding } from "./utiltity.js";
 
 
-
-class Type {
-  constructor() {
-    this.rock = "rock";
-    this.paper = "paper";
-    this.rock = "scissors";
-  }
-}
 export class Choice extends Entity {
   constructor(position) {
     super(position);
     this.color = "gray";
     this.radius = 30;
-    this.velocity = new Velocity(10, 10);
-    this.type = new Type();
+    this.velocity = new Velocity(5, 5);
+    this.type = ["rock", "paper", "scissors"]
     this.rounds = 0;
     this.amount = 3;
   }
@@ -28,13 +20,30 @@ export class Choice extends Entity {
     ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
+
+if (this.type === "rock") {
+addsTextToCanvas(ctx, "ROCK", "20PX", new Position(this.position.x, this.position.y))
+}
+if (this.type === "paper") {
+addsTextToCanvas(ctx, "PAPER", "20PX", new Position(this.position.x, this.position.y))
+}
+if (this.type === "scissors") {
+addsTextToCanvas(ctx, "SCISSORS", "20PX", new Position(this.position.x, this.position.y))
+}
+
+
+
   }
 
   animate(game) {
     let player = game.player;
     let computer = game.computer;
+
+    
 //player choice
+player.type = "paper"
 //computer choice
+computer.type = "rock"
 
 if (this.rounds < this.amount) {
     //move up
@@ -54,13 +63,13 @@ if (this.rounds < this.amount) {
     }
 //create result index
 if (this.rounds === 0 && isColliding(player, computer) && this.position.y === height * 0.9) {
-    game.entities.push(new Result(new Position(this.position.x, this.position.y)))
+    game.entities.push(new Result(new Position(this.position.x, this.position.y), this.type))
 }
 if (this.rounds === 1 &&isColliding(player, computer) && this.position.y === height * 0.8) {
-    game.entities.push(new Result(new Position(this.position.x, this.position.y)))
+    game.entities.push(new Result(new Position(this.position.x, this.position.y), this.type))
 }
 if (this.rounds === 2 && isColliding(player, computer) && this.position.y === height * 0.7) {
-    game.entities.push(new Result(new Position(this.position.x, this.position.y)))
+    game.entities.push(new Result(new Position(this.position.x, this.position.y), this.type))
 }
     //leave canvas, then return
     if (this.position.y >= height + this.radius) {
@@ -74,12 +83,12 @@ if (this.rounds === 2 && isColliding(player, computer) && this.position.y === he
 }
 }
 export class Result extends Entity {
-    constructor(position) {
+    constructor(position, type) {
         super(position);
         this.color = "red";
         this.radius = 30;
         this.velocity = new Velocity(1, 1);
-        this.type = new Type();
+        this.type = type;
       }
     
       draw(ctx) {
@@ -88,6 +97,17 @@ export class Result extends Entity {
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
+
+        if (this.type === "rock") {
+          addsTextToCanvas(ctx, "ROCK", "20PX", new Position(this.position.x, this.position.y))
+          }
+          if (this.type === "paper") {
+          addsTextToCanvas(ctx, "PAPER", "20PX", new Position(this.position.x, this.position.y))
+          }
+          if (this.type === "scissors") {
+          addsTextToCanvas(ctx, "SCISSORS", "20PX", new Position(this.position.x, this.position.y))
+          }
+
       }
       animate(game, ctx) {
    if (game.entities.length >= 8 && this.position.y > height * 0.25) {
@@ -100,10 +120,15 @@ if (game.entities[game.index].position.y === 150) {
 }
 if (game.entities[game.index].color === "rgba(0, 0, 0, 0.0") {
 addsTextToCanvas(ctx, "TBA WINS!", "90px", new Position(width / 2, 150))
-addsTextToCanvas(ctx, "x : y", "70px", new Position(width / 2, 230))
-}
+addsTextToCanvas(ctx, "x : y", "50px", new Position(width / 2, 230))
 
 }
+
+
+
+}
+
+
 
       }
     
