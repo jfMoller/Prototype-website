a742311a03cecb69bd1c9a95d73fb6ff9b91ec1b
Johan <johan.moller040@gmail.com>
2022-11-18 08:54:08 +0100

    clearing practice content

--- menu.js
+++ menu.js
@@ -4,6 +4,8 @@ import { setupGameForPlayer1, setupGameForPlayer2, setupArena } from "./game.js"
 let content = document.getElementById("dynamic_container")
 
 function setupMenu() {
+
+
 let start_button = document.createElement("div");
 
 
@@ -13,11 +15,13 @@ start_button.innerText = "START GAME";
 start_button.setAttribute("id", "start_button")
 
 
+
 start_button.addEventListener( "click", () => {
 startGame();
 })
 
 content.appendChild(start_button);
+
 
 }
 setupMenu();
--- utiltity.js
+++ utiltity.js
@@ -4,23 +4,3 @@ export function clearChildren(node) {
     node.removeChild(node.firstChild);
   }
 }
-
-export function isColliding(circle1, circle2) {
-  let distance = Math.sqrt(
-    Math.pow(circle1.position.x - circle2.position.x, 2) +
-      Math.pow(circle1.position.y - circle2.position.y, 2)
-  );
-  if (distance <= circle1.radius + circle2.radius) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-export function addsTextToCanvas(ctx, text, fontSize, position) {
-    ctx.font = fontSize.concat(" " + "Monospace");
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(text, position.x, position.y);
-  }
